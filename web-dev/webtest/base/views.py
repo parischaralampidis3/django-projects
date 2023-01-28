ff05f576587559cfafdb9dbb6272cf1f30da2173
Paris Charalampidis <curlycode@outlook.com>
2023-01-28 10:41:35 +0200

    creating global templates for app

--- web-dev/webtest/base/views.py
+++ web-dev/webtest/base/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 # Create your views here.
-
+rooms = [
+    {'id':1, 'name':'Lets learn Python'},
+    {'id':2, 'name':'Lets learn SQL'},
+    {'id':3, 'name':'Lets learn R'},
+]
 
 def home(request):
-    return render(request,"./home.html")
+    context = {'rooms':rooms}
+    return render(request,"base/home.html", context)
 
 def room(request):
-    return render(request, "./room.html")
+    return render(request, "base/room.html")
 
+ 
