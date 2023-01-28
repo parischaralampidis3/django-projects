010727f6bd04233e82df78a70aa30c6185f71837
Paris Charalampidis <curlycode@outlook.com>
2023-01-28 15:42:21 +0200

    adding context in template

--- web-dev/webtest/base/views.py
+++ web-dev/webtest/base/views.py
@@ -11,7 +11,7 @@ def home(request):
     context = {'rooms':rooms}
     return render(request,"base/home.html", context)
 
-def room(request):
+def room(request,pk):
     return render(request, "base/room.html")
 
- 
+      
