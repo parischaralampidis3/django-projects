9bdae20ede7154109e02891227dc84d5e07f32a2
Paris Charalampidis <curlycode@outlook.com>
2023-02-01 08:32:43 +0200

    set an app/db gateway

--- web-dev/webtest/base/views.py
+++ web-dev/webtest/base/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
+from .models import Room
 #from django.http import HttpResponse
 # Create your views here.
-rooms = [
-    {'id':1, 'name':'Lets learn Python'},
-    {'id':2, 'name':'Lets learn SQL'},
-    {'id':3, 'name':'Lets learn R'},
-]
+#rooms = [
+#    {'id':1, 'name':'Lets learn Python'},
+#    {'id':2, 'name':'Lets learn SQL'},
+#    {'id':3, 'name':'Lets learn R'},
+#]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,"base/home.html", context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk) :
-            room = i
+    room = Room.objects.get(id=pk)
+    #for i in rooms:
+    #    if i['id'] == int(pk) :
+    #        room = i
     context = {'room':room}
 
     return render(request, "base/room.html",context)
