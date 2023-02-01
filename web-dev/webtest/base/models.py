868a719ad4e0b746b9ee5ec036304a53d04bdcd5
Paris Charalampidis <curlycode@outlook.com>
2023-02-01 09:47:11 +0200

    create model relationships

--- web-dev/webtest/base/models.py
+++ web-dev/webtest/base/models.py
@@ -1,13 +1,29 @@
 from django.db import models
-
-
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Topic(models.Model):
+    #room 
+    name = models.CharField(max_length=200)
+
+def __str__(self):
+    return self.name
+
+
+#CASCADE: When the referenced object is deleted, also delete the objects that have references to it 
+# (when you remove a blog post for instance, you might want to delete comments as well). 
+# SQL equivalent: CASCADE.
+
+
+#SET_NULL: Set the reference to NULL (requires the field to be nullable).
+#  For instance, when you delete a User, you might want to keep the comments he posted on blog posts,
+#  but say it was posted by an anonymous (or deleted) user.
+#  SQL equivalent: SET NULL.
 
 class Room(models.Model):
-    #host
-    #topic
+    host = models.ForeignKey(User, on_delete = models.SET_NULL, null =True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     #null means value can't be blank
     description = models.TextField(null=True, blank =True)
@@ -19,3 +35,24 @@ class Room(models.Model):
 
 def __str__(self):
     return self.name
+
+
+class Message(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #describes the entity we want to point to. on_delete 
+    #instead describes how the database should behave when the "one" side of the relationship is deleted. 
+    # When the "one" entity is deleted, with CASCADE the "many" 
+    # entities are deleted as well.
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.body[0:50] 
+
+
+
+
+    
